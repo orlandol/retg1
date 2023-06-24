ec3cd32b01b646fc2250b4e9bc3a606ec7880418
orlandol <orlandol@veribox.net>
2023-06-24 04:44:56 -0400

    Updated code. Linking in progress.
    - Replaced hard coded asm generation with string variable sections.
    
    - Figuring out bug in linker invocation.

--- build.bat
+++ build.bat
@@ -3,3 +3,4 @@ call clean.bat
 
 3rdparty\packcc\packcc retg1.peg
 if exist retg1.c 3rdparty\tcc\tcc retg1.c
+if exist 3rdparty\verpatch\verpatch.exe if exist retg1.exe 3rdparty\verpatch\verpatch /va retg1.exe
--- mkt.bat
+++ mkt.bat
@@ -1,3 +1,4 @@
 @echo off
-3rdparty\nasm\nasm -fobj test.asm -o t.obj
+3rdparty\nasm\nasm -fobj test.asm -o test.obj
 3rdparty\alink\alink test.obj -oPE -subsys console -o test.exe
+if exist 3rdparty\verpatch\verpatch.exe if exist test.exe 3rdparty\verpatch\verpatch -va test.exe
