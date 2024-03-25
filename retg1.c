a4bb2cabcdbaabad06c2cb7179d6c90be17b12f8
orlandol <orlandol@veribox.net>
2024-03-25 05:55:09 -0400

    Current iteration will be developed using PellesC's IDE for debugging, and building with either PellesC or the TinyC Compiler.

--- retg1.c
+++ retg1.c
@@ -1,6 +1,115 @@
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/*
+ *  String declarations
+ */
+
+typedef struct String {
+  size_t maxLength;
+  size_t length;
+  char data[];
+} String;
+
+String* NewString( size_t maxLength );
+String* DuplicateCString( const char* cstring );
+String* DuplicateString( const String* sourceString );
+unsigned ReleaseString( String** stringPtr );
+
+String* AppendString( String* destString, const char* sourceString );
+
+/*
+ *  Code generator declarations
+ */
+
+// Instruction buffer
+typedef struct X86Instruction {
+  unsigned fieldsUsed;
+} X86Instruction;
+
+typedef struct Executable {
+  FILE* handle;
+} Executable;
+
+Executable* CreateExecutable( const String* fileName );
+unsigned CloseExecutable( Executable** executablePtr );
+
+/*
+ *  Parser declarations
+ */
+typedef struct Parser {
+  FILE* handle;
+} Parser;
+
+Parser* OpenSource( const String* fileName );
+unsigned CloseSource( Parser** parserPtr );
+
+/*
+ *  Main program
+ */
+
+Parser* retFile = NULL;
+Executable* exeFile = NULL;
+
+unsigned InferFileNames( int runArgsCount, const char* runArgs[] );
+
+void Cleanup( void ) {
+  CloseSource( &retFile );
+  CloseExecutable( &exeFile );
+}
 
 int main( int argc, char* argv[] ) {
+  atexit( Cleanup );
+
   return 0;
+}
+
+/*
+ *  String implementation
+ */
+
+String* NewString( size_t maxLength ) {
+  return NULL;
+}
+
+String* DuplicateCString( const char* cstring ) {
+  return NULL;
+}
+
+String* DuplicateString( const String* sourceString ) {
+  return NULL;
+}
+
+unsigned ReleaseString( String** stringPtr ) {
+  return 2;
+}
+
+String* AppendString( String* destString, const char* sourceString ) {
+  return NULL;
+}
+
+/*
+ *  Code generator implementation
+ */
+
+Executable* CreateExecutable( const String* fileName ) {
+  return NULL;
+}
+
+unsigned CloseExecutable( Executable** executablePtr ) {
+  return 2;
+}
+
+/*
+ *  Parser implementation
+ */
+
+Parser* OpenSource( const String* fileName ) {
+  return NULL;
+}
+
+unsigned CloseSource( Parser** parserPtr ) {
+  return 2;
 }
