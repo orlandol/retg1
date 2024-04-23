2e0f1fe1ed7710f1312a983bbd90f8ef7d154ddf
orlandol <orlandol@veribox.net>
2024-04-23 06:18:18 -0400

    Updated tests.
    Updated tests:
    - strings.c Refactored/generalized tests into functions to make tests more readable, as well as to remove duplicate code.

--- strings.c
+++ strings.c
@@ -4,12 +4,85 @@
 
 #include "retlib.h"
 
+unsigned errorCount = 0;
+
+void TestPointer( unsigned testNumber, const char* statement,
+  const void* pointer ) {
+
+  if( pointer == NULL ) {
+    printf( "[%02u] '%s' resulted in a NULL pointer\n",
+      testNumber, statement ? statement : ""
+    );
+    errorCount++;
+  }
+}
+
+void TestValue( unsigned testNumber, const char* statement,
+  unsigned actualValue, unsigned expectedValue ) {
+
+  if( actualValue != expectedValue ) {
+    printf( "[%02u] '%s' resulted in %u instead of %u\n",
+      testNumber, statement ? statement : "",
+      actualValue, expectedValue
+    );
+    errorCount++;
+  }
+}
+
+void TestGreater( unsigned testNumber,
+  const char* leftStatement, unsigned leftValue,
+  const char* rightStatement, unsigned rightValue ) {
+
+  if( leftValue < rightValue ) {
+    printf( "[%02u] '%s':%u should be greater than > '%s':%u\n",
+      testNumber,
+      leftStatement ? leftStatement : "", leftValue,
+      rightStatement ? rightStatement : "", rightValue
+    );
+    errorCount++;
+  }
+}
+
+void TestEqual( unsigned testNumber,
+  const char* leftStatement, unsigned leftValue,
+  const char* rightStatement, unsigned rightValue ) {
+
+  if( leftValue != rightValue ) {
+    printf( "[%02u] '%s':%u should be equal to '%s':%u\n", testNumber,
+      leftStatement, leftValue,
+      rightStatement, rightValue
+    );
+    errorCount++;
+  }
+}
+
+void TestReleaseString( unsigned testNumber, unsigned result,
+  const char* pointerStatement, void* pointer ) {
+
+  switch( result ) {
+  case 0: /* expected */ break;
+  case 1:
+    printf( "[%02u] ReleaseString('%s') received a NULL reference\n",
+      testNumber, pointerStatement );
+    errorCount++;
+    break;
+  default:
+    printf( "[%02u] ReleaseString('%s') returned an unexpected result"
+      "code of %u\n", testNumber, pointerStatement, result );
+    errorCount++;
+  }
+  if( pointer != NULL ) {
+    printf( "[%02u] Expected pointer to be NULL after "
+      "ReleaseString('%s')\n", testNumber, pointerStatement );
+    errorCount++;
+  }
+}
+
 int main( int argc, char* argv[] ) {
   retstring s1 = NULL;
   retstring s2 = NULL;
   retstring tmpstr = NULL;
   unsigned uresult;
-  unsigned errorCount = 0;
   size_t reservedLength = 0;
   size_t length = 0;
   size_t clength = 0;
@@ -44,72 +117,42 @@ int main( int argc, char* argv[] ) {
 
   // NewString(non-zero) test cases
   s1 = NewString(5);
-  if( s1 == NULL ) {
-    printf( "[00] NewString(5) returned a NULL pointer\n" );
-    errorCount++;
-  }
+  TestPointer( 0, "NewString(5)", s1 );
+
   reservedLength = StringReservedLength(s1);
-  if( reservedLength != 8 ) {
-    printf( "[01] StringReservedLength(s1) returned %u instead of %u\n", reservedLength, 8 );
-    errorCount++;
-    printf( "[02] s1->reservedLength is %u instead of %u after NewString(5)\n", reservedLength, 8 );
-    errorCount++;
-  }
+  TestValue( 1, "StringReservedLength(s1)", reservedLength, 8 );
+
   length = StringLength(s1);
-  if( length != 0 ) {
-    printf( "[03] StringLength(s1) retuned %u instead of %u\n", length, 0 );
-    errorCount++;
-    printf( "[04] s1->length is %u instead of %u after NewString(5)\n", length, 0 );
-    errorCount++;
-  }
+  TestValue( 2, "StringLength(s1)", length, 0 );
+
   clength = strlen(s1);
-  if( clength != 0 ) {
-    printf( "[05] strlen(s1) returned %u instead of %u\n", clength, 0 );
-    errorCount++;
-  }
-  if( reservedLength < length ) {
-    printf( "[06] StringReservedLength(s1):%u should be greater than StringLength(s1):%u\n", reservedLength, length );
-    errorCount++;
-  }
-  if( length != clength ) {
-    printf( "[07] StringLength(s1):%u should be equal to strlen(s1):%u\n", length, clength );
-    errorCount++;
-  }
+  TestValue( 3, "strlen(s1)", clength, 0 );
+
+  TestGreater( 4,
+    "StringReservedLength(s1)", reservedLength,
+    "StringLength(s1)", length
+  );
+
+  TestEqual( 5, "StringLength(s1)", length, "strlen(s1)", clength );
 
   // AppendChar test cases
   tmpstr = AppendChar(s1, 'H');
-  if( tmpstr == NULL ) {
-    printf( "[08] AppendChar(s1, 'H') returned a NULL pointer\n" );
-    errorCount++;
-  }
+  TestPointer( 6, "AppendChar(s1, 'H')", tmpstr );
   s1 = tmpstr ? tmpstr : s1;
+
   reservedLength = StringReservedLength(s1);
-  if( reservedLength != 8 ) {
-    printf( "[09] StringReservedLength(s1) returned %u instead of %u\n", reservedLength, 8 );
-    errorCount++;
-    printf( "[10] s1->reservedLength is %u instead of %u after AppendChar(s1, 'H')\n", reservedLength, 8 );
-    errorCount++;
-  }
+  TestValue( 7, "StringReservedLength(s1)", reservedLength, 8 );
+
   length = StringLength(s1);
-  if( length != 1 ) {
-    printf( "[11] StringLength(s1) returned %u instead of %u\n", length, 1 );
-    errorCount++;
-    printf( "[12] s1->length is %u instead of %u after AppendChar(s1, 'H')\n", length, 1 );
-    errorCount++;
-  }
+  TestValue( 8, "StringLength(s1)", length, 1 );
+
   clength = strlen(s1);
-  if( clength != 1 ) {
-    printf( "[13] strlen(s1) returned %u instead of %u\n", clength, 1 );
-    errorCount++;
-  }
-  if( reservedLength < length ) {
-    printf( "[14] StringReservedLength(s1):%u should be greater than StringLength(s1):%u\n", reservedLength, length );
-    errorCount++;
-  }
-  if( length != clength ) {
-    printf( "[15] StringLength(s1):%u should be equal to strlen(s1):%u\n", length, clength );
-    errorCount++;
-  }
+  TestValue( 9, "strlen(s1)", clength, 1 );
+
+  TestGreater( 10, "StringReservedLength(s1)", reservedLength,
+    "StringLength(s1)", length );
+
+  TestEqual( 11, "StringLength(s1)", length, "strlen(s1)", clength );
 
   ///TODO: AppendCString test
   tmpstr = AppendCString(s1, "ello, ");
@@ -123,123 +166,53 @@ int main( int argc, char* argv[] ) {
   s2 = DuplicateCString("orld!");
   tmpstr = AppendString(s1, s2);
   s1 = tmpstr ? tmpstr : s1;
-  ReleaseString( &s2 );
+  uresult = ReleaseString( &s2 );
+  TestReleaseString( 12, uresult, "&s2", s2 );
 
   uresult = ReleaseString(&s1);
-  switch( uresult ) {
-  case 0: /* expected */ break;
-  case 1:
-    printf( "[16] ReleaseString(&s1) received &s1 as a NULL pointer\n" );
-    errorCount++;
-    break;
-  default:
-    printf( "[17] ReleaseString(&s1) returned an unexpected result code of %u\n", uresult );
-    errorCount++;
-  }
-  if( s1 != NULL ) {
-    printf( "[18] Expected s1 to be NULL after ReleaseString(&s1)\n" );
-    errorCount++;
-  }
+  TestReleaseString( 13, uresult, "&s1", s1 );
 
   // DuplicateCString tests
   s1 = DuplicateCString("Hello, world!");
-  if( s1 == NULL ) {
-    printf( "[19] DuplicateCString('Hello, world!') returned a NULL pointer\n" );
-    errorCount++;
-  }
+  TestPointer( 14, "DuplicateCString('Hello, world!')", s1 );
+
   reservedLength = StringReservedLength(s1);
-  if( reservedLength != 16 ) {
-    printf( "[20] StringReservedLength(s1) returned %u instead of %u\n", reservedLength, 16 );
-    errorCount++;
-    printf( "[21] s1->reservedLength is %u instead of %u after DuplicateCString('Hello, world!')\n", reservedLength, 16 );
-    errorCount++;
-  }
+  TestValue( 15, "StringReservedLength(s1)", reservedLength, 16 );
+
   length = StringLength(s1);
-  if( length != 13 ) {
-    printf( "[22] StringLength(s1) returned %u instead of %u\n", length, 13 );
-    errorCount++;
-    printf( "[23] s1->length is %u instead of %u after DuplicateCString('Hello, world!')\n", length, 13 );
-    errorCount++;
-  }
+  TestValue( 16, "StringLength(s1)", length, 13 );
+
   clength = strlen(s1);
-  if( clength != 13 ) {
-    printf( "[24] strlen(s1) returned %u instead of %u\n", clength, 13 );
-    errorCount++;
-  }
-  if( reservedLength < length ) {
-    printf( "[25] StringReservedLength(s1):%u should be greater than StringLength(s1):%u\n", reservedLength, length );
-    errorCount++;
-  }
-  if( length != clength ) {
-    printf( "[26] StringLength(s1):%u should be equal to strlen(s1):%u\n", length, clength );
-    errorCount++;
-  }
+  TestValue( 17, "strlen(s1)", clength, 13 );
+
+  TestGreater( 18, "StringReservedLEngth(s1)", reservedLength,
+    "StringLEngth(s1)", length );
+
+  TestEqual( 19, "StringLength(s1)", length, "strlen(s1)", clength );
 
   // DuplicateString tests
   s2 = DuplicateString(s1);
-  if( s2 == NULL ) {
-    printf( "[27] DuplicateString(s1) returned a NULL pointer\n" );
-    errorCount++;
-  }
+  TestPointer( 20, "DuplicateString(s1)", s2 );
+
   reservedLength = StringReservedLength(s2);
-  if( reservedLength != 16 ) {
-    printf( "[28] StringReservedLength(s2) returned %u instead of %u\n", reservedLength, 16 );
-    errorCount++;
-    printf( "[29] s2->reservedLength is %u instead of %u after DuplicateString(s1)\n", reservedLength, 16 );
-    errorCount++;
-  }
+  TestValue( 21, "StringReservedLength(s2)", reservedLength, 16 );
+
   length = StringLength(s2);
-  if( length != 13 ) {
-    printf( "[30] StringLength(s2) returned %u instead of %u\n", length, 13 );
-    errorCount++;
-    printf( "[31] s2->length is %u instead of %u after DuplicateString(s1)\n", length, 13 );
-    errorCount++;
-  }
+  TestValue( 22, "StringLength(s2)", length, 13 );
+
   clength = strlen(s2);
-  if( clength != 13 ) {
-    printf( "[32] strlen(s2) returned %u instead of %u\n", clength, 13 );
-    errorCount++;
-  }
-  if( reservedLength < length ) {
-    printf( "[33] StringReservedLength(s1):%u should be greater than StringLength(s1):%u\n", reservedLength, length );
-    errorCount++;
-  }
-  if( length != clength ) {
-    printf( "[34] StringLength(s1):%u should be equal to strlen(s1):%u\n", length, clength );
-    errorCount++;
-  }
+  TestValue( 23, "strlen(s2)", clength, 13 );
+
+  TestGreater( 24, "StringReservedLength(s1)", reservedLength,
+    "StringLength(s1)", length );
+
+  TestEqual( 25, "StringLEngth(s1)", length, "strlen(s1)", clength );
 
   uresult = ReleaseString(&s1);
-  switch( uresult ) {
-  case 0: /* expected */ break;
-  case 1:
-    printf( "[35] ReleaseString(&s1) received &s1 as a NULL pointer\n" );
-    errorCount++;
-    break;
-  default:
-    printf( "[36] ReleaseString(&s1) returned an unexpected result code of %u\n", uresult );
-    errorCount++;
-  }
-  if( s1 != NULL ) {
-    printf( "[37] Expected s1 to be NULL after ReleaseString(&s1)\n" );
-    errorCount++;
-  }
+  TestReleaseString( 26, uresult, "&s1", s1 );
 
   uresult = ReleaseString(&s2);
-  switch( uresult ) {
-  case 0: /* expected */ break;
-  case 1:
-    printf( "[38] ReleaseString(&s2) received &s2 as a NULL pointer\n" );
-    errorCount++;
-    break;
-  default:
-    printf( "[39] ReleaseString(&s2) returned an unexpected result code of %u\n", uresult );
-    errorCount++;
-  }
-  if( s2 != NULL ) {
-    printf( "[40] Expected s2 to be NULL after ReleaseString(&s2)\n" );
-    errorCount++;
-  }
+  TestReleaseString( 27, uresult, "&s2", s2 );
 
   if( errorCount ) {
     printf( "[String tests failed with %u errors]\n", errorCount );
