36b4ba379a1e859e774f5720929ccb1a9075da87
Orlando LLanes <orlandol@veribox.net>
2026-03-02 03:40:13 -0600

    Updated source code.
    Added code to retg2.ret for testing and planning purposes.
    
    Added symbol table prototyping code to symtab.c
    - There's currently a bug somewhere in DeclareSymbol.

--- sandbox/atsymtab/symtab.c
+++ sandbox/atsymtab/symtab.c
@@ -5,32 +5,171 @@
 
 #include "avl_tree.h"
 
+typedef void (*ReleaseSymbolFunc)( struct Symbol** symbolPtr );
+
+typedef struct GlobalVarSymbol {
+  unsigned typeSpec;
+} GlobalVarSymbol;
+
 typedef struct Symbol {
+  ReleaseSymbolFunc releaseSymbol;
+
   char* name;
   unsigned symtype;
-  struct avl_tree_node node;
+
+  union {
+    GlobalVarSymbol globalVarSymbol;
+  };
+
+  struct avl_tree_node* node;
 } Symbol;
+
+void ReleaseSymbol( Symbol** symbolPtr ) {
+  Symbol* symbol = NULL;
+
+  if( symbolPtr ) {
+    symbol = (*symbolPtr);
+
+    if( symbol ) {
+      if( symbol->name ) {
+        free( symbol->name );
+        symbol->name = NULL;
+      }
+
+      symbol->symtype = 0;
+    }
+
+    free( (*symbolPtr) );
+    (*symbolPtr) = NULL;
+  }
+}
+
+void ReleaseGlobalVarSymbol( Symbol** symbolPtr ) {
+  Symbol* symbol = NULL;
+
+  if( symbolPtr ) {
+    symbol = (*symbolPtr);
+
+    if( symbol ) {
+      symbol->globalVarSymbol.typeSpec = 0;
+    }
+  }
+
+  ReleaseSymbol( symbolPtr );
+}
 
 typedef struct SymbolTable {
   struct avl_tree_node* root;
   unsigned symCount;
 } SymbolTable;
 
-int compare_nodes( const struct avl_tree_node *leftNode,
-                          const struct avl_tree_node *rightNode ) {
+int CompareSymbols( const struct avl_tree_node *leftNode,
+                   const struct avl_tree_node *rightNode ) {
   Symbol* leftSym = NULL;
   Symbol* rightSym = NULL;
 
-  if( !(leftNode && rightNode) ) { return 254; }
+  if( !(leftNode && rightNode) ) { return 256; }
+printf( "$\n" );
 
   leftSym = avl_tree_entry(leftNode, Symbol, node);
   rightSym = avl_tree_entry(rightNode, Symbol, node);
-  if( !(leftSym && rightSym) ) { return 255; }
+  if( !(leftSym && rightSym) ) { return 257; }
+printf( "leftSym->name == '%s'; rightSym->name == '%s'\n", leftSym->name, rightSym->name );
 
   return strcmp(leftSym->name, rightSym->name);
 }
 
+SymbolTable* CreateSymbolTable() {
+  return calloc(1, sizeof(SymbolTable));
+}
+
+void ReleaseSymbolTable( SymbolTable** symbolTablePtr ) {
+  SymbolTable* symbolTable = NULL;
+  Symbol* curSymbol = NULL;
+  ReleaseSymbolFunc releaseSymbol = NULL;
+
+  if( symbolTablePtr ) {
+    symbolTable = (*symbolTablePtr);
+
+    if( symbolTable ) {
+      avl_tree_for_each_in_postorder( curSymbol,
+          symbolTable->root, Symbol, node ) {
+
+        avl_tree_remove( &(symbolTable->root), curSymbol->node );
+        avl_tree_node_set_unlinked( curSymbol->node );
+
+        releaseSymbol = ReleaseSymbol;
+        if( curSymbol->releaseSymbol ) {
+          releaseSymbol = curSymbol->releaseSymbol;
+        }
+        releaseSymbol( &curSymbol );
+
+        free( curSymbol );
+        curSymbol = NULL;
+      }
+
+      symbolTable->symCount = 0;
+    }
+
+    free( (*symbolTablePtr) );
+    (*symbolTablePtr) = NULL;
+  }
+}
+
+unsigned DeclareSymbol( SymbolTable* symbolTable, Symbol* symbol ) {
+  struct avl_tree_node* existingNode = NULL;
+
+  if( symbolTable == NULL ) { return 1; }
+  if( !(symbol && symbol->name && (*symbol->name)) ) { return 2; }
+
+  existingNode = avl_tree_insert( &(symbolTable->root),
+      symbol->node, CompareSymbols );
+  if( existingNode ) { return 3; }
+
+  return 0;
+}
+
+unsigned DeclareGlobalVar( SymbolTable* symbolTable,
+    const char* varName, unsigned varTypespec ) {
+  Symbol* newSymbol = NULL;
+
+  if( symbolTable == NULL ) { return 1; }
+  if( !(varName && (*varName)) ) { return 2; }
+  if( varTypespec == 0 ) { return 3; }
+
+  newSymbol = calloc(1, sizeof(Symbol));
+  if( newSymbol == NULL ) { return 4; }
+
+  newSymbol->releaseSymbol = ReleaseGlobalVarSymbol;
+  newSymbol->name = strdup(varName);
+  newSymbol->globalVarSymbol.typeSpec = varTypespec;
+
+  if( DeclareSymbol(symbolTable, newSymbol) ) {
+    ReleaseGlobalVarSymbol( &newSymbol );
+
+    return 5;
+  }
+
+  return 0;
+}
+
+SymbolTable* symtab = NULL;
+
+void Cleanup() {
+  ReleaseSymbolTable( &symtab );
+}
+
 int main( int argc, char** argv ) {
+  atexit( Cleanup );
+
+  symtab = CreateSymbolTable();
+
+  printf( "DeclareGlobalVar(...'test'...) == %u\n", DeclareGlobalVar( symtab, "test", 1234 ) );
+
+  Cleanup();
+
+  printf( "Done\n" );
+
   return 0;
 }
 
